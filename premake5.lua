fc96ddc9442a222af5c3db9101983044fc53b5ac
torq <quezado_8588@hotmail.com>
2018-12-18 09:01:48 -0200

    Added premake file
    This is for the Hazel engine build system

---
+++ premake5.lua
@@ -0,0 +1,29 @@
+project "ImGui"
+    kind "StaticLib"
+    language "C++"
+
+    -- outputdir should come from the workspace premake file
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+        "imgui_demo.cpp"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+        cppdialect "C++17"
+        staticruntime "On"
+
+    filter { "system:windows", "configurations:Release" }
+        buildoptions "/MT"
